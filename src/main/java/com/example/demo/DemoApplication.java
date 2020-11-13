9ad0e41fc1d5e0b35c5a95619b2341650e17b4f9
sarahcabral <sarah.ly2009@gmail.com>
2020-11-12 23:44:16 -0300

    deletando anotações

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,37 +11,3 @@ public class DemoApplication {
 	}
 
 }
-/*
-1. Construa uma API RestFul com SpringBoot, seguindo os modelos e a arquitetura 
-de sistema apresentados durante a disciplina. 
-
- 
-
-2. Esse projeto terá 2 modelos. Temos os seguintes modelos: Escola e  Curso
-
-Uma Escola possui vários Cursos e um Curso só pode ser de uma Escola.
-
-Defina pelo menos 5 atributos para cada uma das classes acima a sua escolha. 
-O código de cada modelo deverá ser gerado automaticamente. 
-
- 
-
-3. Implemente as seguintes funcionalidades (endpoints) nos Controladores:
-
-Escola:
-
-Listar todas as escolas
-Listar uma escola pelo código
-Cadastrar uma escola
-Remover uma escola (somente se ela não tiver cursos)
-Alterar os dados de uma escola (Somente 2 dados/atributos podem ser alterados)
-Listar todos os cursos de uma escola
-
-Curso:
-
-Cadastrar um curso (sempre associar o curso a uma escola).
-Listar todos os cursos.
-Listar um curso pelo código.
-Remover um curso
-Alterar um curso (somente 1 dado do curso poderá ser alterado).
-*/
